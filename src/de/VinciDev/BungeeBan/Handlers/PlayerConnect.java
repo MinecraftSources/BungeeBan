ad0081a4de0382a3267501e3d03374660c7378ba
NubeBuster <kiwiszijncool@gmail.com>
2016-01-18 13:59:49 +0100

    Missing updates from the last commit
    some stuff that didnt update correctly

--- src/de/VinciDev/BungeeBan/Handlers/PlayerConnect.java
+++ src/de/VinciDev/BungeeBan/Handlers/PlayerConnect.java
@@ -1,6 +1,6 @@
 package de.vincidev.bungeeban.handlers;
 
-import de.vincidev.bungeeban.util.BungeeBan;
+import de.vincidev.bungeeban.utils.BungeeBanUtils;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -10,6 +10,6 @@ public class PlayerConnect implements Listener {
 	@EventHandler
 	public void onConnected(ServerConnectEvent e) {
 		ProxiedPlayer p = e.getPlayer();
-		BungeeBan.pushPlayer(p);
+		BungeeBanUtils.pushPlayer(p);
 	}
 }
